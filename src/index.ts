eae9910be80f12b033656ac99203f91f22440e0e
Allen <1130123899@qq.com>
2024-03-11 13:43:56 +0800

    test:  a new function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import process from 'node:process'
-import type {
-  MainLogger,
-  NodeLogger,
-  RendererLogger,
-  Variables,
-} from 'electron-log'
-import log from 'electron-log'
+import MainLog from 'electron-log/main'
+import RendererLog from 'electron-log/renderer'
+import NodeLog from 'electron-log/node'
+
+import { isMain, isRenderer } from './is'
 import type { LogOptions } from './type'
 import { normalizeConfig, resolveConfig } from './config'
 import { getElectronLogVersion } from './utils'
-import { isMain } from './is'
 
-let hasInit = false
 export async function unLogger(
-  logger: MainLogger | RendererLogger | NodeLogger,
   { cwd = process.cwd() } = {},
 ) {
   const config = await normalizeConfig(await resolveConfig({ cwd }))
@@ -21,59 +16,26 @@ export async function unLogger(
   // eslint-disable-next-line no-console
   console.log(`Now electron-log version is ${await getElectronLogVersion()}`)
 
-  if ((logger.variables as Variables).processType === 'main') {
-    (logger as MainLogger).initialize()
+  if (isRenderer)
+    return RendererLog
+
+  if (isMain) {
+    MainLog.initialize()
 
     if (config.size)
-      (logger as MainLogger).transports.file.maxSize = config.size
+      MainLog.transports.file.maxSize = config.size
 
     if (config.resolvePathFn)
-      (logger as MainLogger).transports.file.resolvePathFn = config.resolvePathFn
+      MainLog.transports.file.resolvePathFn = config.resolvePathFn
 
     if (config.archiveLogFn)
-      (logger as MainLogger).transports.file.archiveLogFn = config.archiveLogFn
+      MainLog.transports.file.archiveLogFn = config.archiveLogFn
+
+    return MainLog
   }
-
-  if ((logger.variables as Variables).processType === 'renderer') {
-    // eslint-disable-next-line no-console
-    console.log('renderer')
-  }
-
-  if ((logger.variables as Variables).processType === 'browser') {
-    // eslint-disable-next-line no-console
-    console.log('browser')
-  }
-
-  // Object.assign(logger, config)
-
-  return logger
+  else { return NodeLog }
 }
 
 export function defineConfig(config: Partial<LogOptions>) {
   return config
 }
-
-(async function initialize() {
-  // eslint-disable-next-line no-console
-  console.log('@dmzj/electron-log-plugin work')
-  if (isMain && !hasInit) {
-    const config = await normalizeConfig(await resolveConfig({ cwd: process.cwd() }))
-    // eslint-disable-next-line no-console
-    console.log(
-      'test',
-      process.cwd(),
-    )
-    if (config.size)
-      log.transports.file.maxSize = config.size
-
-    if (config.resolvePathFn)
-      log.transports.file.resolvePathFn = config.resolvePathFn
-
-    if (config.archiveLogFn)
-      log.transports.file.archiveLogFn = config.archiveLogFn
-
-    log.initialize()
-
-    hasInit = true
-  }
-})()
